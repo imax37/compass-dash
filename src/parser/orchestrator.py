081c4b392415d411244e0f4a3bd83e81f3432e51
ianm99 <umeabor@student.ubc.ca>
2026-04-01 12:41:44 -0700

    refactored pdf parsing logic and updated notebook

---
+++ src/parser/orchestrator.py
@@ -0,0 +1,28 @@
+from .pdf_reader import extract_words_from_pdf
+from .extractors import(
+    get_date_columns,
+    find_employee_rows,
+    get_employee_shifts,
+    match_shift_types,
+    assign_dates
+)
+
+def parse_pdf(pdf_path):
+    all_results = []
+
+    for words in extract_words_from_pdf(pdf_path):
+
+        date_columns = get_date_columns(words)
+        employee_rows = find_employee_rows(words)
+
+        shifts = get_employee_shifts(
+            words, employee_rows['shiftTimeRow']
+            )
+        shifts_w_type = match_shift_types(
+            shifts, words, employee_rows['shiftTypeRow']
+            )
+        results = assign_dates(shifts_w_type, date_columns)
+
+        all_results.extend(results)
+
+    return all_results
