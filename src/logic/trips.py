044b25c2b511dd021de241c611aeb91ce8f4429e
ianm99 <umeabor@student.ubc.ca>
2026-04-22 12:37:37 -0700

    refactor: move trip counting logic into src modules

--- src/logic/trips.py
+++ src/logic/trips.py
@@ -12,3 +12,21 @@ def ShiftsToTrips(df):
                         value_name='time')
     
     return trips.sort_values('date')
+
+def GetOneZoneTrips(df):
+    "Find all trips that occur on a weekend or after 6:30pm"
+    one_zone = df.loc[(df['is_weekend']==True) | (df['time']>1830)]
+
+    return one_zone
+
+def GetTwoZoneTrips(df):
+    "find all trips that occur on a weekday before 6:30pm"
+    two_zone = df.loc[(df['is_weekend']==False) & (df['time']<1830)]
+
+    return two_zone
+
+def GetAddFareTrips(df):
+    "find all trips that will incur the YVR add fare"
+    add_fare = df.loc[df['origin'] == 'YVR']
+
+    return add_fare
