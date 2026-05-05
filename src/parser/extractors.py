29617e3bac1a93beceb9d61e58808555c192615d
ianm99 <umeabor@student.ubc.ca>
2026-05-04 18:55:09 -0700

    fix: moved changes to current branch to avoid accidental commit to main

--- src/parser/extractors.py
+++ src/parser/extractors.py
@@ -45,31 +45,15 @@ def find_employee_rows(words, first="Ian", last="Maccarthy", y_tol=20, x_tol=20)
         "row_max": lastName['top']
     }
 
-<<<<<<< HEAD
-# SHIFT_PATTERN = re.compile(r"\d{4}-\d{4}")
-SHIFT_PATTERN = re.compile(r"(\d{4}-\d{4})")
-
-def get_employee_shifts(words, employee_y, tolerance=8):
-=======
 
 SHIFT_PATTERN = re.compile(r"(\d{4}-\d{4})")
 
 def get_employee_shifts(words, row_bottom, row_top, tolerance=5):
->>>>>>> c9aa30f (fix: updated parsing logic to use employee row rather than type and time rows)
     shifts = []
     row_min = min(row_top, row_bottom) - tolerance
     row_max = max(row_top, row_bottom) + tolerance
 
     for w in words:
-<<<<<<< HEAD
-        if abs(w['top'] - employee_y) < tolerance:
-            # if SHIFT_PATTERN.match(w['text']):
-            match = SHIFT_PATTERN.search(w['text'])
-            if match:
-                #shifts.append({'x':w['x0'], 'time':match.group(1), 'raw': w['text']})
-                shifts.append(w)
-    print(shifts[:2])
-=======
         
         if row_min <= w['top'] <= row_max:
 
@@ -77,7 +61,6 @@ def get_employee_shifts(words, row_bottom, row_top, tolerance=5):
             if match:
                 shifts.append(w)
     
->>>>>>> c9aa30f (fix: updated parsing logic to use employee row rather than type and time rows)
     return shifts
 
 def match_shift_types(shifts, words, row_bottom, row_top, y_tol=5, x_tol=50):
@@ -88,31 +71,19 @@ def match_shift_types(shifts, words, row_bottom, row_top, y_tol=5, x_tol=50):
     for shift in shifts:
         x = shift['x0']
 
-        print("SHIFT TYPE ROW TARGET:", shift_type_y)
-    
-        # --- Old format separate line ---
         for w in words:
-            if abs(w['x0'] - x) < 5:  # very tight x match
-                print(f"Candidate near x={x}: text={w['text']}, top={w['top']}")
             if (
-<<<<<<< HEAD
-                abs(w['top'] - shift_type_y) < y_tol and 
-                abs(w['x0'] - x) < x_tol 
-                and not SHIFT_PATTERN.search(w['text'])
-=======
                 row_min <= w['top'] <= row_max and 
                 abs(w['x0'] - x) < x_tol and
                 not SHIFT_PATTERN.search(w['text'])
->>>>>>> c9aa30f (fix: updated parsing logic to use employee row rather than type and time rows)
             ):
                 results.append({
                     'x': x,
-                    'time': shift['time'],
+                    'time': shift['text'],
                     'type': w['text']
                 })
                 break
 
-    print(f'results have length {len(results)}')
     return results
 
 def assign_dates(shifts, date_columns):
